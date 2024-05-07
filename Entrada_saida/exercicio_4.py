38153c7aaa1a057c8455a8cfefb9f1581b47d14f
Juhtrento <julianelli.d.trento@gmail.com>
2024-05-07 11:04:08 -0300

    média.py

---
+++ Entrada_saida/exercicio_4.py
@@ -0,0 +1,6 @@
+print(f"Aqui calculamos a sua média!!!")
+nota1 = float(input("Digite a primeira nota:"))
+nota2 = float(input("Digite a segunda nota:"))
+nota3 = float(input("Digite a terceira nota:"))
+media = (nota1+nota2+nota3)/3
+print(f"A sua média final é: {media}")
