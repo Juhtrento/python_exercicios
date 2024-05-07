e0b66a1f0fc5e5892caf7e143a78a19e142fd002
Juhtrento <julianelli.d.trento@gmail.com>
2024-05-07 09:36:32 -0300

    soma_prod.py

---
+++ Entrada_saida/exercicio_1.py
@@ -0,0 +1,10 @@
+print(f"Bem-vindo(a) a calculadora!!!")
+num1 = int(input("Digite o primeiro número:"))
+num2 = int(input("Digite o segundo número:"))
+soma= num1+num2
+print(f"A soma dos dois números é: {soma}")
+multiplicação = num1*num2
+print(f"O produto dos números é: {multiplicação}")
+
+
+
